71232ff76295f6b6be5e48ff4c14166f0b64c9ac
rajiv <2238388+rajivghanta@users.noreply.github.com>
2021-02-02 10:10:38 -0500

    making URL and title not unique

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from app import db
 
 class Job(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	url = db.Column(db.String(1024), index=True, unique=True)
-	title = db.Column(db.String(1024), index=True, unique=True)
+	url = db.Column(db.String(1024), index=True)
+	title = db.Column(db.String(1024), index=True)
 
 	def __repr__(self):
 		return '<Job {}>'.format(self.title)
